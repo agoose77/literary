cfe9d459a5fdeac283d2eecb09f131a660c7e861
Angus Hollands <goosey15@gmail.com>
2020-10-30 15:36:41 +0000

    Refactor: only pass LiteraryPythonPreprocessor by default

--- literary/exporter.py
+++ literary/exporter.py
@@ -7,9 +7,7 @@ from .preprocessor import LiteraryPythonPreprocessor
 class LiteraryPythonExporter(exporters.PythonExporter):
     @traitlets.default("default_preprocessors")
     def _default_preprocessors_default(self):
-        return exporters.PythonExporter.default_preprocessors.default() + [
-            LiteraryPythonPreprocessor
-        ]
+        return [LiteraryPythonPreprocessor]
 
     @traitlets.default("exclude_input_prompt")
     def _exclude_input_prompt_default(self):
