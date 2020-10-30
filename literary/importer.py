0223206dc8600845a14b874e271e4faeaa73c0ad
Angus Hollands <goosey15@gmail.com>
2020-10-30 15:36:24 +0000

    Bugfix: support loading packages

--- literary/importer.py
+++ literary/importer.py
@@ -1,6 +1,7 @@
 import importlib.machinery
 import os
 import pathlib
+import linecache
 import sys
 from typing import AnyStr, Union
 
@@ -37,11 +38,21 @@ class NotebookLoader(importlib.abc.SourceLoader):
     def get_filename(self, full_name):
         return str(self.path)
 
-    def get_data(self, path):
+    def update_linecache(self, path: str, source: str):
+        linecache.cache[path] = (
+            len(source),
+            None,
+            source.splitlines(keepends=True),
+            path,
+        )
+
+    def get_data(self, path: str):
         c = traitlets.config.Config()
         nb = nbformat.read(path, as_version=nbformat.NO_CONVERT)
         exporter = LiteraryPythonExporter(config=c)
         body, resources = exporter.from_notebook_node(nb)
+        # Ensure that generated source is available for tracebacks
+        self.update_linecache(path, body)
         return body.encode()
 
 
@@ -52,21 +63,33 @@ class NotebookFinder(importlib.abc.MetaPathFinder):
     def find_spec(self, fullname, path, target=None):
         if path is None:
             path = self._path
-
         head, _, module_name = fullname.rpartition(".")
 
         for entry in path:
-            notebook_path = pathlib.Path(entry) / f"{module_name}.ipynb"
-            if notebook_path.exists():
+            # If we can find {module_name}/__init__.ipynb, this is a package
+            package_path = pathlib.Path(entry) / module_name / "__init__.ipynb"
+            if package_path.exists():
                 spec = importlib.machinery.ModuleSpec(
-                    fullname, NotebookLoader(notebook_path), origin=str(notebook_path)
+                    fullname,
+                    NotebookLoader(package_path),
+                    origin=str(package_path),
+                    is_package=True,
+                )
+                spec.submodule_search_locations = [str(package_path.parent)]
+                spec.has_location = True
+                return spec
+
+            # Otherwise look for the notebook itself
+            module_path = pathlib.Path(entry) / f"{module_name}.ipynb"
+            if module_path.exists():
+                spec = importlib.machinery.ModuleSpec(
+                    fullname, NotebookLoader(module_path), origin=str(module_path)
                 )
                 spec.has_location = True
-                # spec.parent =
                 return spec
 
 
 def install_hook(path=None):
     if path is None:
         path = sys.path
-    sys.meta_path.append(NotebookFinder(path))
+    sys.meta_path.insert(0, NotebookFinder(path))
