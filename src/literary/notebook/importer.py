4d326dcf22aa971e3bca8e435407f34066af81c3
Angus Hollands <goosey15@gmail.com>
2021-05-06 21:08:19 +0100

    Refactor: use path_hooks instead of meta_path
    Fixes #11

--- src/literary/notebook/importer.py
+++ src/literary/notebook/importer.py
@@ -1,10 +1,11 @@
 import importlib.machinery
+import inspect
 import linecache
 import os
 import pathlib
 import sys
 import traceback
-from typing import AnyStr, Union
+from typing import AnyStr, Any, Callable, Tuple, TypeVar, Union
 
 import nbformat
 
@@ -13,10 +14,12 @@ from ..exporter import LiteraryPythonExporter
 
 
 class NotebookLoader(importlib.machinery.SourcelessFileLoader):
+    """Sourceless Jupyter Notebook loader"""
+
     def __init__(self, fullname: str, path: str, config):
         super().__init__(fullname, path)
 
-        self.config = config
+        self._config = config
 
     def _update_linecache(self, path: str, source: str):
         linecache.cache[path] = (
@@ -35,53 +38,9 @@ class NotebookLoader(importlib.machinery.SourcelessFileLoader):
 
     def get_transpiled_source(self, path: str):
         nb = nbformat.read(path, as_version=nbformat.NO_CONVERT)
-        exporter = LiteraryPythonExporter(config=self.config)
+        exporter = LiteraryPythonExporter(config=self._config)
         body, resources = exporter.from_notebook_node(nb)
         return body
-
-
-class NotebookFinder(importlib.abc.MetaPathFinder):
-    def __init__(self, path, config):
-        self._path = path
-        self._config = config
-
-    def _load_package_spec(
-        self, path: pathlib.Path, name: str
-    ) -> importlib.machinery.ModuleSpec:
-        spec = importlib.machinery.ModuleSpec(
-            name,
-            NotebookLoader(name, str(path), self._config),
-            origin=str(path),
-            is_package=True,
-        )
-        spec.submodule_search_locations = [str(path.parent)]
-        spec.has_location = True
-        return spec
-
-    def _load_module_spec(
-        self, path: pathlib.Path, name: str
-    ) -> importlib.machinery.ModuleSpec:
-        spec = importlib.machinery.ModuleSpec(
-            name, NotebookLoader(name, str(path), self._config), origin=str(path)
-        )
-        spec.has_location = True
-        return spec
-
-    def find_spec(self, fullname, path, target=None):
-        if path is None:
-            path = self._path
-        head, _, module_name = fullname.rpartition(".")
-
-        for entry in path:
-            # If we can find {module_name}/__init__.ipynb, this is a package
-            package_path = pathlib.Path(entry) / module_name / "__init__.ipynb"
-            if package_path.exists():
-                return self._load_package_spec(package_path, fullname)
-
-            # Otherwise look for the notebook itself
-            module_path = pathlib.Path(entry) / f"{module_name}.ipynb"
-            if module_path.exists():
-                return self._load_module_spec(module_path, fullname)
 
 
 def determine_package_name(path: pathlib.Path, package_root_path: pathlib.Path) -> str:
@@ -94,6 +53,68 @@ def determine_package_name(path: pathlib.Path, package_root_path: pathlib.Path) 
     """
     relative_path = path.relative_to(package_root_path)
     return ".".join(relative_path.parts)
+
+
+def _get_loader_details(hook) -> tuple:
+    """Return the loader_details for a given FileFinder closure
+
+    :param hook: FileFinder closure
+    :returns: loader_details tuple
+    """
+    try:
+        namespace = inspect.getclosurevars(hook)
+    except TypeError as err:
+        raise ValueError from err
+
+    try:
+        return namespace.nonlocals["loader_details"]
+    except KeyError as err:
+        raise ValueError from err
+
+
+def _find_file_finder(path_hooks: list) -> Tuple[int, Any]:
+    """Find the FileFinder closure in a list of path hooks
+
+    :param path_hooks: path hooks
+    :returns: index of hook and the hook itself
+    """
+    for i, hook in enumerate(path_hooks):
+        try:
+            _get_loader_details(hook)
+        except ValueError:
+            continue
+
+        return i, hook
+    raise ValueError
+
+
+T = TypeVar("T")
+
+
+def _extend_file_finder(finder: T, *loader_details) -> T:
+    """Extend an existing file finder with new loader details
+
+    :param finder: existing FileFinder instance
+    :param loader_details:
+    :return:
+    """
+    return importlib.machinery.FileFinder.path_hook(
+        *_get_loader_details(finder), *loader_details
+    )
+
+
+def _inject_notebook_loader(
+    path_hooks: list, loader_factory: Callable[[str, str], NotebookLoader]
+):
+    """Inject a NotebookLoader into a list of path hooks
+
+    :param path_hooks: list of path hooks
+    :param loader_factory: factory to to create NotebookLoader
+    :return:
+    """
+    i, finder = _find_file_finder(path_hooks)
+    new_finder = _extend_file_finder(finder, (loader_factory, [".ipynb"]))
+    path_hooks[i] = new_finder
 
 
 def install_hook(
@@ -109,11 +130,18 @@ def install_hook(
     inside notebooks
     :return:
     """
+    # Load config for project
+    config = load_config(package_root_path)
+
     # Make notebook packages importable by adding package root path to sys.path
     sys.path.append(str(package_root_path))
 
-    config = load_config(package_root_path)
-    sys.meta_path.insert(0, NotebookFinder(sys.path, config=config))
+    # Create notebook loader factory
+    def create_notebook_loader(fullname, path):
+        return NotebookLoader(fullname, path, config)
+
+    # Inject notebook loader into path_hooks
+    _inject_notebook_loader(sys.path_hooks, create_notebook_loader)
 
     # Python's C-level traceback reporting doesn't call `linecache`, and so retrieves
     # the underlying notebook source instead of the generated Python code
